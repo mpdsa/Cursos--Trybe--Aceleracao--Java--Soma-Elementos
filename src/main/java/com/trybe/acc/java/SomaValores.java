af59f70dfdd6c69982a219b18298c6c34d311f06
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-29 18:12:19 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/SomaValores.java
+++ src/main/java/com/trybe/acc/java/SomaValores.java
@@ -3,25 +3,33 @@ package com.trybe.acc.java;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * The type Soma valores.
+ */
 public class SomaValores {
-	
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
-    // Ler a entrada de um número pelo console
+    int[] myArray = new int [3];
+    int soma = 0;
+
     Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-    int[] myArray = new int [3]; 
-
-    // Laço de repetição 
     for (int index = 0; index < 3; index++) {
       System.out.println("Digite um número:");
       myArray[index] = scan.nextInt();
     }
 
-    // Seu código aqui
-
+    for (int count : myArray) {
+      soma = soma + count;
+    }
 
     // Imprime a saída no console. OBS: descomente a próxima linha
-    //System.out.println("A soma é: " + soma);
+    System.out.println("A soma é: " + soma);
 
   }
 
